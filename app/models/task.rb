1b713625b02d75f3f94baa2c0498bb4b1eda3b53
Rabab Zaitouni <rabab.zaitouni@gmail.com>
2018-12-05 16:20:48 +0100

    seeds almost done for users, projects, and tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :project
   belongs_to :user
-  belongs_to :client_review, :class_name => 'Review', :foreign_key => 'client_review_id', optional: true
-  belongs_to :expert_review, :class_name => 'Review', :foreign_key => 'expert_review_id', optional: true
+  belongs_to :client_review, class_name: 'Review', foreign_key: 'client_review_id', optional: true, dependent: :destroy
+  belongs_to :expert_review, class_name: 'Review', foreign_key: 'expert_review_id', optional: true, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroying all industries...'
+Industry.destroy_all
+puts 'Destroying all users...'
+User.destroy_all
+puts 'Destroying all projects...'
+Project.destroy_all
+
+# Managers
+3.times do
+  user = User.create(
+    first_name: Faker::Name::first_name,
+    last_name: Faker::Name::last_name,
+    email: Faker::Internet::email,
+    password: "123456",
+    role: 'manager'
+  )
+  puts "User #{user.email} (#{user.role}) created"
+end
+
+# Clients
+5.times do
+  user = User.create(
+    first_name: Faker::Name::first_name,
+    last_name: Faker::Name::last_name,
+    email: Faker::Internet::email,
+    password: "123456",
+    role: 'client'
+  )
+  puts "User #{user.email} (#{user.role}) created"
+end
+
+# Experts
+10.times do
+  user = User.create(
+    first_name: Faker::Name::first_name,
+    last_name: Faker::Name::last_name,
+    email: Faker::Internet::email,
+    password: "123456",
+    role: 'expert'
+  )
+  puts "User #{user.email} (#{user.role}) created"
+end
+
+10.times do
+  project = Project.create(
+    title: Faker::Simpsons.quote,
+    description: Faker::Simpsons.quote,
+    status: false,
+    start_date: Date.today,
+    deadline: Date.today + rand(1..5).year,
+    client: User.where(role: 'client').sample,
+    manager: User.where(role: 'manager').sample
+  )
+  puts "Project #{project.title} created"
+
+  rand(5..10).times do
+    task = Task.create(
+      title: Faker::Simpsons.quote,
+      project: project,
+      user: User.where(role: 'expert').sample
+    )
+    puts "Task #{task.title} created for #{project.title}"
+  end
+end
