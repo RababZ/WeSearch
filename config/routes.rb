e73676bde7519d889ab7ee67371a0963aeeadd15
Mehdi Ritab <ritabmehdi@gmail.com>
2018-12-06 15:53:09 +0100

    TaskB Policy

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :projects do
-    resources :tasks, only: [ :index, :new, :create, :edit, :update]
+    resources :tasks, only: [ :index, :new, :create, :edit, :update, :destroy]
   end
-  resources :tasks, only: [ :show, :destroy]
+  # resources :tasks, only: [ :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
