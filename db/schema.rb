e9fbcc4b8e73e58c38afdf5957ee9ee7316aff59
Mehdi Ritab <ritabmehdi@gmail.com>
2018-12-12 16:25:41 +0100

    i dont know

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_12_12_102053) do
-
 ActiveRecord::Schema.define(version: 2018_12_12_103756) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +30,13 @@ ActiveRecord::Schema.define(version: 2018_12_12_103756) do
   end
 
   create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "conversation_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
